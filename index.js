3fdac80cf213653054250d94129e1f48d50d3075
agodse21 <agodse9@gmail.com>
2022-11-04 12:08:23 +0530

    about added

--- index.js
+++ index.js
@@ -7,6 +7,9 @@ app.use(express.json());
 app.get("/",(req,res)=>{
     res.send("hello this is home")
 })
+app.get("/about",(req,res)=>{
+    res.send("hello this is about")
+})
 app.listen(PORT,()=>{
     
     console.log(`Lising on port ${PORT}`)
