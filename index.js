a09d16b2b61c8b310dd3ee6592344787d3525259
agodse21 <agodse9@gmail.com>
2022-11-04 11:53:40 +0530

    backenf

--- index.js
+++ index.js
@@ -4,7 +4,9 @@ require("dotenv").config();
 const PORT=process.env.PORT || 8080
 app.use(express.json());
 
-
+app.get("/",(req,res)=>{
+    res.send("hello this is home")
+})
 app.listen(PORT,()=>{
     
     console.log(`Lising on port ${PORT}`)
